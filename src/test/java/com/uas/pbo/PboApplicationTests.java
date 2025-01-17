5f9716532c12d327376804f9cda2f30405f32fab
alianhar <mamankkalop939@gmail.com>
2025-01-18 04:41:11 +0700

    init

---
+++ src/test/java/com/uas/pbo/PboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uas.pbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
